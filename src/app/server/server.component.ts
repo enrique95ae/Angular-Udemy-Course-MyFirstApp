73129b744163fb130d50b110f90dcbd46e6af790
Enrique Alonso Esposito <enrique95ae@gmail.com>
2023-01-08 15:59:03 +0100

    ngStyle
    Dynamically sytling elements

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ColdObservable } from "rxjs/internal/testing/ColdObservable";
 
 @Component({
     selector: 'app-server',
@@ -10,7 +11,15 @@ export class ServerComponent
     serverId: number = 10;
     serverStatus: string = 'offline';
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offiline';
+    }
+
     getServerStatus() {
         return this,this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
